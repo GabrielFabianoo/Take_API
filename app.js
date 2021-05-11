4ad8ef2a68d41600fc7f229893bb968997dd823d
gabrielfabianoo91 <gabrielfabianoo91@gmail.com>
2021-05-10 23:57:44 -0300

    fix: main route

--- app.js
+++ app.js
@@ -6,5 +6,10 @@ const repoRoutes = require("./routes/repoRoutes");
 
 app.use("/user", userRoutes);
 app.use("/repo", repoRoutes);
+app.use("/", (req, res, next) => {
+    res.status(200).send({
+        data: "you should try to use this https://api-take-chatbot.herokuapp.com/user or this https://api-take-chatbot.herokuapp.com/repo",
+    });
+});
 
 module.exports = app;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require("axios").default;
 
 router.get("/", (req, res, next) => {
-    const user = axios.get("https://api.github.com/users/takenet").then((response) => {
+    axios.get("https://api.github.com/users/takenet").then((response) => {
         res.status(200).send({
             data: response.data.avatar_url,
         });
